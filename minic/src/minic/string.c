51d47252e8dbbc4d9d91b24ead738ba50f38dfdf
Charles Waldner <waldner.charles@gmail.com>
2019-05-17 17:22:50 -0600

    Refactor string.c to remove goto

--- minic/src/minic/string.c
+++ minic/src/minic/string.c
@@ -6,29 +6,29 @@ u32
 string_length(const char *str)
 {
     u32 n = 0;
-    while (cast(ptr_t, str) & 3)
-    {
-    null_check:
-        if (*str == 0)
-        {
-            return n;
-        }
-
-        ++str;
-		++n;
-    }
-
+    bool unaligned = cast(ptr_t, str) & 3;
     for (;;)
     {
         u32 word = *cast(u32 *, str);
         u32 has_null = (word - 0x01010101) & ((~word) & 0x80808080);
 
-        if (has_null)
+        if (unaligned || has_null)
         {
-			goto null_check;
-		}
+            do
+            {
+                if (*str == 0)
+                {
+                    return n;
+                }
 
-		str += 4;
-        n += 4;
+                ++str;
+                ++n;
+            } while ((unaligned = cast(ptr_t, str) & 3))
+        }
+        else
+        {
+            str += 4;
+            n += 4;
+        }
     }
 }
