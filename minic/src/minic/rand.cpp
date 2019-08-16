ee343a72abeab1eb467540525594a259a7d8013f
Charles Waldner <waldner.charles@gmail.com>
2019-08-15 20:20:47 -0600

    Formatting

--- minic/src/minic/rand.cpp
+++ minic/src/minic/rand.cpp
@@ -2,8 +2,7 @@
 #include "minic/rand.h"
 #include "minic/int.h"
 
-static u64
-rand_splitmix64(u64 val)
+static u64 rand_splitmix64(u64 val)
 {
     val += 0x9e3779b97f4a7c15;
     val = (val ^ (val >> 30)) * 0xbf58476d1ce4e5b9;
@@ -11,8 +10,7 @@ rand_splitmix64(u64 val)
     return val ^ (val >> 31);
 }
 
-static u64
-rand_xoroshiro128plus(u64 s[2])
+static u64 rand_xoroshiro128plus(u64 s[2])
 {
     u64 s0 = s[0];
     u64 s1 = s[1];
@@ -25,15 +23,13 @@ rand_xoroshiro128plus(u64 s[2])
     return result;
 }
 
-void
-rand_seed(Random *rand, u64 seed)
+void rand_seed(Random *rand, u64 seed)
 {
     rand->s[0] = rand_splitmix64(seed);
     rand->s[1] = rand_splitmix64(rand->s[0]);
 }
 
-u64
-rand_next_u64(Random *rand)
+u64 rand_next_u64(Random *rand)
 {
     return rand_xoroshiro128plus(rand->s);
 }
